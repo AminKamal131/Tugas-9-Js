fa6e6a94a06e84c472e5ec4dec36b3656c924447
Amin Kamal <aminkamal355@gmail.com>
2022-12-07 21:16:54 +0800

    Tugas 9
    Pengumpulan Tugas

---
+++ tugas9.js
@@ -0,0 +1,13 @@
+function amin(){
+    var kamal = {
+        Namalengkap : "Amin Kamal",
+        umur : 20,
+        Pekerjaan : "kuli Panggul",
+        TinggiBadan : 178,
+    }
+    for(var AK in kamal){
+        console.log(kamal[AK])
+    }
+}
+
+amin()
